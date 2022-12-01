b471be6b19ae7ffda3d323a63287d8f8c9a0c5d9
Rariolu <rcoyneyr82011@gmail.com>
2022-12-01 18:22:14 +0000

    Implemented solution to part 2

--- 1/code.js
+++ 1/code.js
@@ -4,11 +4,11 @@ const fsConfig = {encoding:"utf8",flag:"r"};
 
 console.log("Bloop.");
 
-function GetMaxCalories(inputStr)
+function GetTopThreeMaxCalories(inputStr)
 {
     let calorieTotals = [];
     let elfInventories = inputStr.split("\r\n\r\n");
-    let maxCalories = 0;
+    let topThree = [0,0,0];
     for (let i in elfInventories)
     {
         let inventory = elfInventories[i];
@@ -20,12 +20,28 @@ function GetMaxCalories(inputStr)
             total += parseInt(calorieCount);
         }
         calorieTotals.push(total);
-        if (total > maxCalories)
+        updateTopThree(topThree, total);
+    }
+    return topThree;
+}
+
+function updateTopThree(topThree, newValue)
+{
+    let currentIndex = 3;
+    for (let i = 2; i > -1; i--)
+    {
+        if (newValue > topThree[i])
         {
-            maxCalories = total;
+            let temp = topThree[i];
+            topThree[i] = newValue;
+            if (currentIndex < 3)
+            {
+                topThree[currentIndex] = temp;
+            }
+            currentIndex = i;
         }
     }
-    return maxCalories;
+    return topThree;
 }
 
 function main()
@@ -34,8 +50,10 @@ function main()
     const realPath = "./puzzleinput.txt";
 
     const inputStr = fs.readFileSync(realPath, fsConfig);
-    let maxCalories = GetMaxCalories(inputStr);
-    console.log(`Elf with most calories has ${maxCalories}.`);
+    let topThree = GetTopThreeMaxCalories(inputStr);
+    console.log(`Elf with most calories has ${topThree[0]}.`);
+    console.log(`The top three calorie totals are ${topThree[0]}, ${topThree[1]}, and ${topThree[2]}.`);
+    console.log(`The total of the top three is: ${topThree[0] + topThree[1] + topThree[2]}.`);
 }
 
 main();
